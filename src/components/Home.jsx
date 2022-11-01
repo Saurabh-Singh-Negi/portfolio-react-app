ed320e94537cb54099848b71c50cf4e965856a73
Saurabh <saurabhnegi0007@gmail.com>
2022-11-01 23:55:52 +0530

    styling home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import {hisArrowNarrowRight} from "react-icons/hi";
 
 const Home = () => {
     return (
-        <div className='bg-[#0a192f] text-black py-100px'>Home</div>
+        <div className='w-full h-screen bg-[#0a192f] text-black py-100px'>Home</div>
     )
 }
 
