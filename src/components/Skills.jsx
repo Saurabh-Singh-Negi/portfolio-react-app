f34887cb3bbeed0f29e8ddd20f000bc775d28d14
Saurabh <saurabhnegi0007@gmail.com>
2022-11-04 18:07:46 +0530

    added new component Skills

---
+++ src/components/Skills.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Skills = () => {
+    return (
+        <div name="skills" className='w-full h-screen bg-[#0a192f] text-gray-300'>Skills</div>
+    )
+}
+
+
+export default Skills;
