226f39783ce845d8bb96f19ff7a26b5767bf197c
Saurabh <saurabhnegi0007@gmail.com>
2022-11-07 14:12:30 +0530

    added responsive project section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 function App() {
   return (
     <>
@@ -9,6 +10,7 @@ function App() {
       <Home />
       <About />
       <Skills />
+      <Projects />
     </>
   );
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,133 @@
+import React from 'react';
+import shopify from '../assets/work/shopify_clone.png';
+import minion from '../assets/work/minion_translate.png';
+
+const Projects = () => {
+    return (
+        <div name='work' className='bg-[#0a192f] w-full md:h-screen text-gray-300'>
+            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+                <div className='pb-8'>
+                    <p className='text-4xl fonT-bold inline border-b-4 text-gray-300 border-[#FF5858]'>Projects</p>
+                    <p className='py-6'>Check out what I've been working on</p>
+                </div>
+
+                {/* container */}
+                <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+                    {/* Grid Item */}
+                    <div style={{backgroundImage: `url(${shopify})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                Shopify Clone
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href="https://shopifyclone-saurabh.netlify.app/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-;g'>Demo</button>
+                                </a>
+                                <a href="https://github.com/Saurabh-Singh-Negi/Shopify-Clone">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-;g'>Code</button>
+                                </a>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div style={{backgroundImage: `url(${minion})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                Minion Language Translator
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href="https://englishtominionspeak.netlify.app/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-;g'>Demo</button>
+                                </a>
+                                <a href="https://github.com/Saurabh-Singh-Negi/neogcamp-banana-speak">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-;g'>Code</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Grid Item */}
+                    <div style={{backgroundImage: `url(${shopify})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                Emoji Identifier
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href="https://lwjc43.csb.app/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-;g'>Demo</button>
+                                </a>
+                                <a href="https://codesandbox.io/s/react-saurabh-lwjc43?file=/src/App.js">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-;g'>Code</button>
+                                </a>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div style={{backgroundImage: `url(${minion})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                Shopify Clone
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href="/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-;g'>Demo</button>
+                                </a>
+                                <a href="/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-;g'>Code</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Grid Item */}
+                    <div style={{backgroundImage: `url(${shopify})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                Shopify Clone
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href="/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-;g'>Demo</button>
+                                </a>
+                                <a href="/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-;g'>Code</button>
+                                </a>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div style={{backgroundImage: `url(${minion})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                Shopify Clone
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href="/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-;g'>Demo</button>
+                                </a>
+                                <a href="/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-;g'>Code</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Projects;
