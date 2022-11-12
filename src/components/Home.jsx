c2d8505f507477f24ee0bf19e779a42d63fe6de2
Saurabh <saurabhnegi0007@gmail.com>
2022-11-12 13:00:15 +0530

    react-scroll added to home button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,41 @@
-import React from 'react';
-import {HiArrowNarrowRight} from "react-icons/hi";
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
 
 const Home = () => {
-    return (
-        <div name='home' className='w-full h-screen bg-[#0a192f] text-black py-100px px-0 md:px-10 lg:px-0'>Home
+  return (
+    <div
+      name="home"
+      className="w-full h-screen bg-[#0a192f] text-black py-100px px-0 md:px-10 lg:px-0"
+    >
+      Home
+      {/* container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-[#FF5858]">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Saurabh Singh Negi
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a Front End Developer
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          Tech enthusiast who loves to create user friendly products and explore
+          new technologies
+        </p>
 
-            {/* container */}
-            <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-                <p className='text-[#FF5858]'>Hi, my name is</p>
-                <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Saurabh Singh Negi</h1>
-                <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Front End Developer</h2>
-                <p className='text-[#8892b0] py-4 max-w-[700px]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero, quas! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, numquam!</p>
-                <div>
-                    <button className='text-white group border-2 px-6 py-3 my-2 flex gap-3 items-center hover:bg-[#FF5858] hover:border-[#FF5858]'>View Work 
-                        <span className='group-hover:rotate-90 duration-300'>
-                            <HiArrowNarrowRight/>
-                        </span>
-                    </button>
-                </div>
-            </div>
-
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex gap-3 items-center hover:bg-[#FF5858] hover:border-[#FF5858]">
+            <Link to="work" smooth={true} duration={500}>
+              View Work
+            </Link>
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight />
+            </span>
+          </button>
         </div>
-        
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
