df1ee37c3b1c1c62e34922d9091107f9b59cdb29
Saurabh <saurabhnegi0007@gmail.com>
2023-02-09 11:26:17 +0530

    home page sidebar changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -99,12 +99,12 @@ const Navbar = () => {
                              <HiMail size={30}/>
                         </a>
                     </li>
-                    <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-red-500">
+                    {/* <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-red-500">
                         <a className="flex justify-between items-center w-full text-gray-300 "
                         href="#">
                             Resume <BsFillPersonLinesFill size={30}/>
                         </a>
-                    </li>
+                    </li> */}
                 </ul>
             </div>
         
